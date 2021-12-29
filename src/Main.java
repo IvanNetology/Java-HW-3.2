ea45545236c127abc41c9e85416916c463e769d1
IvanNetology <ivan.ivan.91.91@internet.ru>
2021-12-29 21:57:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double mass = 83;  // масса в кг.
+        double height = 1.75;  // рост в метрах
+        double bmi = service.calculate(mass, height);
+        System.out.println("Индекс массы тела: " + bmi);
+
+    }
+}
